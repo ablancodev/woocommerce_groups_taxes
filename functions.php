798924b76df2fc3a0181d2a2250992c1e3854e1d
eggemplo <eggemplo@gmail.com>
2016-03-03 09:53:24 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+function eggemplo_woocommerce_product_tax_class( $tax_class, $product ) {
+	// you can get this value from Groups->Groups table
+	$group_id = 2;
+	
+	// get user first group
+	$user_id = get_current_user_id();
+	
+	$exists = Groups_User_Group::read( $user_id, $group_id );
+	if ( $exists ) {
+		$tax_class = 'Zero Rate';
+	}
+	return $tax_class;
+}
+add_filter( 'woocommerce_product_tax_class', 'eggemplo_woocommerce_product_tax_class', 1, 2 );
